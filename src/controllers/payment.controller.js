b67bd5b4abf4371fc455bf760c81dfaefde01129
Ssanti66 <santiagosaucedo66@gmail.com>
2023-05-23 23:37:33 -0300

    ticket generation update

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,10 +1,13 @@
+import Stripe from "stripe";
+import dotenv from "dotenv";
+import __dirname from "../dirname.js";
 import { CartService } from "../repository/index.js";
+import { ProductService } from "../repository/index.js";
 import { UserService } from "../repository/index.js";
 import { TicketService } from "../repository/index.js";
-import { ProductService } from "../repository/index.js";
-import { generateRandomString } from "../public/js/generateRandomString.js";
-import CustomError from "../services/errors/customError.js";
-import { ERRORS_ENUM } from "../consts/ERRORS.js";
+dotenv.config();
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const completePayment = async (req, res, next) => {
   const { cid } = req.body;
@@ -38,9 +41,42 @@ export const completePayment = async (req, res, next) => {
       cancel_url: "http://localhost:8082/api/payments/cancel",
     });
 
+    try {
+      const user = await UserService.getUserByCartId(cid);
+
+      const productChecks = await Promise.all(
+        cartRequested.products.map(async (product) => {
+          await Promise.all([
+            CartService.deleteProduct(cid, product.product),
+            ProductService.decreaseStock(product.product, product.quantity),
+          ]);
+          return subtotal;
+        })
+      );
+
+      const ticketCreated = await TicketService.create(
+        {
+            code: generateRandomString(),
+            purchase_datetime: new Date(),
+            amount: totalCart,
+            purchaser: user.email,
+          }
+      )
+      req.logger.info("Generated Ticket: ", ticketCreated);
+
+      const purchasedProductsComplete = await Promise.all(
+        cartRequested.products.map(async (e) => {
+          const product = await ProductService.getById(e.product);
+          return product;
+        })
+      );
+
+    } catch (error) {
+      req.logger.error(error);
+    }
+
     res.send({ status: "success", payload: session });
   } catch (error) {
-    console.error(error);
     res
       .status(500)
       .send({ status: "error", error: "Failed to create Checkout Session" });
