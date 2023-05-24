b5f28735282cda03d79f2e828080113d7099f1e4
Ssanti66 <santiagosaucedo66@gmail.com>
2023-05-24 01:12:50 -0300

    afterPayment updated

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -5,11 +5,15 @@ import { CartService } from "../repository/index.js";
 import { ProductService } from "../repository/index.js";
 import { UserService } from "../repository/index.js";
 import { TicketService } from "../repository/index.js";
+import { isTokenProcessed } from "../utils/utils.js";
+import { markTokenAsProcessed } from "../utils/utils.js";
+import { generateToken } from "../utils/utils.js";
 dotenv.config();
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const completePayment = async (req, res, next) => {
+  const user = req.user.data;
   const { cid } = req.body;
   try {
     const cartRequested = await CartService.getById(cid);
@@ -21,6 +25,8 @@ export const completePayment = async (req, res, next) => {
     }
 
     const totalCart = await CartService.getTotal(cid);
+
+    const token = generateToken(user);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -37,48 +43,89 @@ export const completePayment = async (req, res, next) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:8082/api/payments/success/${cid}`,
+      success_url: `http://localhost:8082/api/payments/success/${cid}?token=${token}`,
       cancel_url: "http://localhost:8082/api/payments/cancel",
     });
 
-    try {
-      const user = await UserService.getUserByCartId(cid);
+    // try {
+    //   const user = await UserService.getUserByCartId(cid);
 
-      const productChecks = await Promise.all(
-        cartRequested.products.map(async (product) => {
-          await Promise.all([
-            CartService.deleteProduct(cid, product.product),
-            ProductService.decreaseStock(product.product, product.quantity),
-          ]);
-          return subtotal;
-        })
-      );
+    //   const productChecks = await Promise.all(
+    //     cartRequested.products.map(async (product) => {
+    //       await Promise.all([
+    //         CartService.deleteProduct(cid, product.product),
+    //         ProductService.decreaseStock(product.product, product.quantity),
+    //       ]);
+    //       return subtotal;
+    //     })
+    //   );
 
-      const ticketCreated = await TicketService.create(
-        {
-            code: generateRandomString(),
-            purchase_datetime: new Date(),
-            amount: totalCart,
-            purchaser: user.email,
-          }
-      )
-      req.logger.info("Generated Ticket: ", ticketCreated);
+    //   const ticketCreated = await TicketService.create(
+    //     {
+    //         code: generateRandomString(),
+    //         purchase_datetime: new Date(),
+    //         amount: totalCart,
+    //         purchaser: user.email,
+    //       }
+    //   )
+    //   req.logger.info("Generated Ticket: ", ticketCreated);
 
-      const purchasedProductsComplete = await Promise.all(
-        cartRequested.products.map(async (e) => {
-          const product = await ProductService.getById(e.product);
-          return product;
-        })
-      );
+    //   const purchasedProductsComplete = await Promise.all(
+    //     cartRequested.products.map(async (e) => {
+    //       const product = await ProductService.getById(e.product);
+    //       return product;
+    //     })
+    //   );
 
-    } catch (error) {
-      req.logger.error(error);
-    }
+    // } catch (error) {
+    //   req.logger.error(error);
+    // }
 
     res.send({ status: "success", payload: session });
   } catch (error) {
     res
       .status(500)
       .send({ status: "error", error: "Failed to create Checkout Session" });
+  }
+};
+
+export const afterPayment = async (req, res, next) => {
+  const paymentToken = req.query.token;
+  const cid = req.params.cid;
+  const cart = await CartService.getById(cid);
+  const user = await UserService.getUserByCartId(cid);
+  const totalCart = await CartService.getTotal(cid);
+
+  if (isTokenProcessed(paymentToken)) {
+    const ticket = await TicketService.getlast(user.email)
+    res.render("successPayment", ticket);
+  } else {
+    try {
+      await Promise.all(
+        cart.products.map(async (product) => {
+          await Promise.all([
+            CartService.deleteProduct(cid, product.product),
+            ProductService.decreaseStock(product.product, product.quantity),
+          ]);
+        })
+      );
+      const purchasedProductsComplete = await Promise.all(
+        cart.products.map(async (e) => {
+          const product = await ProductService.getById(e.product);
+          return product;
+        })
+      );
+      const ticketCreated = await TicketService.create({
+        code: generateRandomString(),
+        purchase_datetime: new Date(),
+        amount: totalCart,
+        purchaser: user.email,
+        products: purchasedProductsComplete
+      });
+      res.render("successPayment", ticketCreated);
+      req.logger.info("Generated Ticket: ", ticketCreated);
+    } catch (error) {
+      req.logger.error(error);
+    }
   }
 };
